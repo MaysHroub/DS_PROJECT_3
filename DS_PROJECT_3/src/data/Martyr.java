1f2ae8495bddf7bc92acdc3b8ae640fe288074da
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 00:40:58 +0300

    add properties of martyr class

--- DS_PROJECT_3/src/data/Martyr.java
+++ DS_PROJECT_3/src/data/Martyr.java
@@ -1,6 +1,10 @@
 package data;
 
 public class Martyr implements Comparable<Martyr> {
+	
+	private String name, district, location;
+	private char gender;
+	private int age;
 
 	@Override
 	public int compareTo(Martyr o) {
