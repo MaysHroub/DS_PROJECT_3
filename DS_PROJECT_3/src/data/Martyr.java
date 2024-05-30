b50b8086b196d8550661d220cdd62bd785b3a447
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 00:39:15 +0300

    override compareTo method

--- DS_PROJECT_3/src/data/Martyr.java
+++ DS_PROJECT_3/src/data/Martyr.java
@@ -1,5 +1,10 @@
 package data;
 
-public class Martyr implements Comparable<T> {
+public class Martyr implements Comparable<Martyr> {
+
+	@Override
+	public int compareTo(Martyr o) {
+		return 0;
+	}
 
 }
