ebdbe59842048594986afaafcb081ce631178e19
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 00:43:05 +0300

    add setters and getters for data fields

--- DS_PROJECT_3/src/data/Martyr.java
+++ DS_PROJECT_3/src/data/Martyr.java
@@ -7,7 +7,50 @@ public class Martyr implements Comparable<Martyr> {
 	private int age;
 	
 	public Martyr() {}
+	
+	public int getAge() {
+		return age;
+	}
 
+	public void setAge(int age) {
+		if (age < 0) throw new IllegalArgumentException("Invalid age value");
+		this.age = age;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		if (gender != 'F' && gender != 'M') 
+			throw new IllegalArgumentException("We don't allow other genders here hhhh :)");
+		this.gender = gender;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
 	@Override
 	public int compareTo(Martyr o) {
 		return 0;
