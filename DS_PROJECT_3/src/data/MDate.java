714562e109349a81ae92b4b10d58657c2ad5ce46
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 00:53:39 +0300

    generate getters and setters for fields of MDate

--- DS_PROJECT_3/src/data/MDate.java
+++ DS_PROJECT_3/src/data/MDate.java
@@ -8,6 +8,23 @@ public class MDate implements Comparable<MDate> {
 	
 	private Date date;
 	private AVLTree<Martyr> martyrs;
+	
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public AVLTree<Martyr> getMartyrs() {
+		return martyrs;
+	}
+
+	public void setMartyrs(AVLTree<Martyr> martyrs) {
+		this.martyrs = martyrs;
+	}
 
 	@Override
 	public int compareTo(MDate o) {
