73e2f845274aad1bc2daf4ce83e7779f30a14c70
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 00:56:24 +0300

    add classes to implement quadratic hash function

---
+++ DS_PROJECT_3/src/hash/OpenAddressignHash.java
@@ -0,0 +1,61 @@
+package hash;
+
+public abstract class OpenAddressignHash<T extends Comparable<T>> {
+	
+	protected HNode<T>[] table;
+	protected int m, size, collisions;
+	
+	@SuppressWarnings("unchecked")
+	protected OpenAddressignHash(int dataSize) {
+		m = dataSize * 2;
+		for(; !isPrime(++m); );
+		table = new HNode[m];
+		for (int i = 0; i < m; i++)
+			table[i] = new HNode<>(null);
+	}
+	
+	public int getCollisions() {
+		return collisions;
+	}
+
+	public abstract void add(T data);
+	
+	public abstract HNode<T> find(T data);
+	
+	public HNode<T> delete(T data) {
+		HNode<T> deleted = find(data);
+		if (deleted != null) deleted.setFlag(Flag.DELETED);
+		--size;
+		return deleted;
+	}
+	
+	private boolean isPrime(int n) {
+		for (int i = 2; i * i <= n; i++)
+			if (n % i == 0) return false;
+		return true;
+	}
+	
+	public void traverse() {
+		for (int i = 0; i < m; i++) 
+			if (table[i].getData() != null)
+				System.out.print(i + "" + table[i] + " - ");
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected void rehash() {
+		int tempM = m;
+		HNode<T>[] tempTable = table;
+		
+		m *= 2;
+		for(; !isPrime(++m); );
+		table = new HNode[m];
+		for (int i = 0; i < m; i++)
+			table[i] = new HNode<>(null);
+		size = 0;
+
+		for (int i = 0; i < tempM; i++) 
+			if (tempTable[i].getFlag() == Flag.FULL)
+				add(tempTable[i].getData());
+	}
+
+}
---
+++ DS_PROJECT_3/src/hash/QuadraticOHash.java
@@ -0,0 +1,41 @@
+package hash;
+
+public class QuadraticOHash<T extends Comparable<T>>  extends OpenAddressignHash<T> {
+	
+	public QuadraticOHash(int dataSize) {
+		super(dataSize);
+	}
+	
+	@Override
+	public void add(T data) {
+		if (size == m/2) rehash();
+		int index = Math.abs(data.hashCode()) % m;
+		int i = 0;
+		for (; table[(index + i * (i++)) % m].getFlag() != Flag.EMPTY && i <= m; collisions++);
+		if (i > m) // a loop occurred
+			System.out.println("This element cannot be added");
+		else {
+			index = (index + (--i) * i) % m;
+			table[index].setData(data);
+			table[index].setFlag(Flag.FULL);
+			++size;
+		}
+	}
+	
+	@Override
+	public HNode<T> find(T data) {
+		int index = Math.abs(data.hashCode()) % m;
+		int i = 0; Flag flag = table[(index + i*i) % m].getFlag();
+		
+		for (; flag == Flag.DELETED ||
+				flag == Flag.FULL && table[(index + i*i) % m].getData().compareTo(data) != 0
+				&& i <= m; // to avoid entering an infinite loop
+				flag = table[(index + (++i) * i) % m].getFlag());
+		
+		if (flag == Flag.FULL && table[(index + i*i) % m].getData().compareTo(data) == 0)
+			return table[(index + i*i) % m];
+		
+		return null;
+	}
+	
+}
