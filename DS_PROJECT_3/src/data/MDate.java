20ac33b5571988c0af28706790f95c4359344a53
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 00:51:57 +0300

    add properties for MDate

--- DS_PROJECT_3/src/data/MDate.java
+++ DS_PROJECT_3/src/data/MDate.java
@@ -1,6 +1,13 @@
 package data;
 
+import java.util.Date;
+
+import tree.AVLTree;
+
 public class MDate implements Comparable<MDate> {
+	
+	private Date date;
+	private AVLTree<Martyr> martyrs;
 
 	@Override
 	public int compareTo(MDate o) {
