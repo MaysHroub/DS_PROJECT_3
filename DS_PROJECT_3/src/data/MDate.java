934f38d2a8bbbf350cc3634d105c4b1aa9c75e03
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 00:58:06 +0300

    implement compareTo method

--- DS_PROJECT_3/src/data/MDate.java
+++ DS_PROJECT_3/src/data/MDate.java
@@ -32,8 +32,7 @@ public class MDate implements Comparable<MDate> {
 
 	@Override
 	public int compareTo(MDate o) {
-		// TODO Auto-generated method stub
-		return 0;
+		return date.compareTo(o.date);
 	}
 
 }
