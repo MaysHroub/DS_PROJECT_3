129000a5d59eefb3f2bede4652499ee34870285b
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 19:15:04 +0300

    create a class which represents the layout to modify date records
    It extends TabLayout class

---
+++ DS_PROJECT_3/src/layout/ModifyDateLayout.java
@@ -0,0 +1,24 @@
+package layout;
+
+import dataholder.DataHolder;
+import javafx.scene.layout.Pane;
+
+public class ModifyDateLayout extends TabLayout {
+
+	public ModifyDateLayout(DataHolder dataHolder) {
+		super("", dataHolder);
+	}
+
+	@Override
+	protected Pane createLayout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateContent() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
