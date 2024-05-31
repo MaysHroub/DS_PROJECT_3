19af62dedb97dd49278830c3009ef31d1c35267f
Mays-Alreem <hroubmays@gmail.com>
2024-05-31 19:24:22 +0300

    declare a combo-box holding dates

--- DS_PROJECT_3/src/layout/ModifyDateLayout.java
+++ DS_PROJECT_3/src/layout/ModifyDateLayout.java
@@ -1,12 +1,15 @@
 package layout;
 
+import data.MDate;
 import dataholder.DataHolder;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.layout.Pane;
 
 public class ModifyDateLayout extends TabLayout {
 	
 	private DatePicker datePicker;
+	private ComboBox<MDate> datesCB;
 
 	public ModifyDateLayout(DataHolder dataHolder) {
 		super("", dataHolder);
