6e1d735a285eecb4019d51b2dfe9238e35f8572e
Mays-Alreem <hroubmays@gmail.com>
2024-06-02 14:43:45 +0300

    add label fields for statistical information of martyrs

--- DS_PROJECT_3/src/layout/NavigateDateLayout.java
+++ DS_PROJECT_3/src/layout/NavigateDateLayout.java
@@ -1,9 +1,13 @@
 package layout;
 
 import dataholder.DataHolder;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 public class NavigateDateLayout extends TabLayout {
+	
+	private Label totalL, totalMalesL, totalFemalesL, avgAgesL,
+					youngestMartyrL, oldestMartyrL, maxMartyrDistrictL, maxMartyrLocationL;
 
 	public NavigateDateLayout(DataHolder dataHolder) {
 		super("Date Navigation", dataHolder);
