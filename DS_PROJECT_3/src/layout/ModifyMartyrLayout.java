d0f0f2fbb3a386f8571b1b99e6056bb4e36ff1b7
Mays-Alreem <hroubmays@gmail.com>
2024-06-02 17:07:38 +0300

    add ModifyMartyrLayout class extending TabLayout to modify martyrs' data

---
+++ DS_PROJECT_3/src/layout/ModifyMartyrLayout.java
@@ -0,0 +1,19 @@
+package layout;
+
+import javafx.scene.layout.Pane;
+
+public class ModifyMartyrLayout extends TabLayout {
+
+	@Override
+	protected Pane createLayout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateContent() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
