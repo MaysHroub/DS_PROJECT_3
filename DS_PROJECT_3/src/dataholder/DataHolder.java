5f559494f2ccc172cd1495154d40cb6360b6b136
Mays-Alreem <hroubmays@gmail.com>
2024-06-03 23:12:54 +0300

    add a setter for dates field

--- DS_PROJECT_3/src/dataholder/DataHolder.java
+++ DS_PROJECT_3/src/dataholder/DataHolder.java
@@ -26,6 +26,10 @@ public class DataHolder {
 		return dates;
 	}
 	
+	public void setDates(QuadraticOHash<MDate> dates) {
+		this.dates = dates;
+	}
+
 	public void moveUp() {
 		int i = currentIdx;
 		for (; --i >= 0 && dates.get(i).getFlag() != Flag.FULL;);
