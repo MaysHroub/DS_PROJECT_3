768db91f5259db2a6f532ec6309ca073df8e52a8
Mays-Alreem <hroubmays@gmail.com>
2024-06-07 11:11:05 +0300

    create a getter and setter for index field

--- DS_PROJECT_3/src/data_structs/hash/HNode.java
+++ DS_PROJECT_3/src/data_structs/hash/HNode.java
@@ -36,4 +36,12 @@ public class HNode<T extends Comparable<T>> {
 	public void setData(T data) {
 		this.data = data;
 	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
 }
