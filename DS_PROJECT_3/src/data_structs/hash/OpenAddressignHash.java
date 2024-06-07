7639f9ee3888d1f1c50f53d11725acd80ec04862
Mays-Alreem <hroubmays@gmail.com>
2024-06-07 11:15:03 +0300

    create HNode objects using constructor with index argument

--- DS_PROJECT_3/src/data_structs/hash/OpenAddressignHash.java
+++ DS_PROJECT_3/src/data_structs/hash/OpenAddressignHash.java
@@ -60,7 +60,7 @@ public abstract class OpenAddressignHash<T extends Comparable<T>> {
 		for(; !isPrime(++m); );
 		table = new HNode[m];
 		for (int i = 0; i < m; i++)
-			table[i] = new HNode<>(null);
+			table[i] = new HNode<>(i);
 		availableData = 0;
 
 		for (int i = 0; i < tempM; i++) 
