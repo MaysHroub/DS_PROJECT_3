0b57947e4da31f2d4a673086b07cf8537b766214
Mays-Alreem <hroubmays@gmail.com>
2024-06-09 22:11:33 +0300

    implement a new method to find districtWithMaxMartyr

--- DS_PROJECT_3/src/data/MDateStat.java
+++ DS_PROJECT_3/src/data/MDateStat.java
@@ -10,48 +10,58 @@ public class MDateStat implements Stat {
 	private int totalMartyrs, totalFemales, totalMales;
 	private int sumAges;
 	private Martyr youngest, oldest;
-	private String districtWithMaxMartyr, locationWithMaxMartyr;
+	private String districtWithMaxMartyr, locationWithMaxMartyr, prevDistrict;
 	
-	private LinkedList<String> districts, locations;
+	private LinkedList<String> locations;
+	
+	private int currDistrictCount, maxDistrictCount;
 	
 	
 	public MDateStat(MDate date) {
 		this.date = date;
-		districts = new LinkedList<>();
 		locations = new LinkedList<>();
 	}
 	
 	private void traverseMartyrs() {
 		totalMartyrs = totalMales = totalFemales = sumAges = 0;
-		districts.clear(); locations.clear();
-		districtWithMaxMartyr = ""; locationWithMaxMartyr = "";
-		if (date.getMartyrs().getRoot() == null) return;
-		youngest = oldest = date.getMartyrs().getRoot().getData();
+		locations.clear();
+		locationWithMaxMartyr = "";
+		districtWithMaxMartyr = "";
+		currDistrictCount = maxDistrictCount = 0;
+		TNode<Martyr> curr = date.getMartyrs().getRoot();
+		if (curr == null) return;
+		youngest = oldest = curr.getData();
+		for (; curr.getLeft() != null; curr = curr.getLeft());
+		prevDistrict = curr.getData().getDistrict();
 		traverseMartyrs(date.getMartyrs().getRoot());
-		findDistrictWithMaxMartyr();
+		if (maxDistrictCount < currDistrictCount) {
+			System.out.println(prevDistrict);
+			districtWithMaxMartyr = prevDistrict;
+			maxDistrictCount = currDistrictCount;
+		}
 		findLocationWithMaxMartyr();
 	}
-
-	private void findDistrictWithMaxMartyr() {
-		Node<String> curr = districts.getHead();
-		if (curr == null) return;
-		districtWithMaxMartyr = curr.getData();
-		int maxCount = 1, currCount = 1;
-		while (curr.getNext() != null) {
-			if (curr.getData().compareTo(curr.getNext().getData()) == 0) 
-				currCount++;
-			else if (maxCount < currCount) {
-				maxCount = currCount;
-				currCount = 1;
-				districtWithMaxMartyr = curr.getData();
-			}
-			curr = curr.getNext();
-		}
-		if (maxCount < currCount) {
-			maxCount = currCount;
-			districtWithMaxMartyr = curr.getData();
-		}
-	}
+	
+//	private void findDistrictWithMaxMartyr() {
+//		Node<String> curr = districts.getHead();
+//		if (curr == null) return;
+//		districtWithMaxMartyr = curr.getData();
+//		int maxCount = 1, currCount = 1;
+//		while (curr.getNext() != null) {
+//			if (curr.getData().compareTo(curr.getNext().getData()) == 0) 
+//				currCount++;
+//			else if (maxCount < currCount) {
+//				maxCount = currCount;
+//				currCount = 1;
+//				districtWithMaxMartyr = curr.getData();
+//			}
+//			curr = curr.getNext();
+//		}
+//		if (maxCount < currCount) {
+//			maxCount = currCount;
+//			districtWithMaxMartyr = curr.getData();
+//		}
+//	}
 	
 	private void findLocationWithMaxMartyr() {
 		Node<String> curr = locations.getHead();
@@ -61,10 +71,12 @@ public class MDateStat implements Stat {
 		while (curr.getNext() != null) {
 			if (curr.getData().compareTo(curr.getNext().getData()) == 0) 
 				currCount++;
-			else if (maxCount < currCount) {
-				maxCount = currCount;
+			else {
+				if (maxCount < currCount) {
+					maxCount = currCount;
+					locationWithMaxMartyr = curr.getData();
+				}
 				currCount = 1;
-				locationWithMaxMartyr = curr.getData();
 			}
 			curr = curr.getNext();
 		}
@@ -77,6 +89,7 @@ public class MDateStat implements Stat {
 	private void traverseMartyrs(TNode<Martyr> curr) {
 		if (curr == null) return;
 		traverseMartyrs(curr.getLeft());
+		
 		totalMartyrs++;
 		Martyr m = curr.getData();
 		if (m.getGender() == 'F') totalFemales++;
@@ -84,8 +97,20 @@ public class MDateStat implements Stat {
 		if (youngest.getAge() > m.getAge()) youngest = m;
 		if (oldest.getAge() < m.getAge()) oldest = m;
 		sumAges += m.getAge();
-		districts.insertSorted(m.getDistrict());
+		
 		locations.insertSorted(m.getLocation());
+
+		if (m.getDistrict().compareTo(prevDistrict) == 0) 
+			currDistrictCount++;
+		
+		else {
+			if (maxDistrictCount < currDistrictCount) {
+				districtWithMaxMartyr = prevDistrict;
+				maxDistrictCount = currDistrictCount;
+			}
+			currDistrictCount = 1;
+		}
+		prevDistrict = m.getDistrict();
 		traverseMartyrs(curr.getRight());
 	}
 
